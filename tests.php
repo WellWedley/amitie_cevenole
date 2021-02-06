58c2c57c9884d0ab54be6ed9c963df9022ca1eaa
WellWedley <footwedley@gmail.com>
2021-02-06 01:43:49 +0100

    Revert "Revert "Footer""
    This reverts commit ba74bb7913f7b6e459588e056b5df065a6c0cb43.

--- tests.php
+++ tests.php
@@ -1,48 +1,52 @@
 <!DOCTYPE html>
 <html lang="fr">
 
-   
+
 <head>
-    <?php
-    include_once 'head.php';
-    ?>
-    <!--<link rel="stylesheet" href="./styles/style.css">-->
+  <?php
+  include_once 'head.php';
+  ?>
+  <!--<link rel="stylesheet" href="./styles/style.css">-->
 </head>
 
 <body>
 
-<div class="month">
-  <ul>
-    <li class="prev">&#10094;</li>
-    <li class="next">&#10095;</li>
-    <li>August<br><span style="font-size:18px">2017</span></li>
+  <div class="month">
+    <ul>
+      <li class="prev">&#10094;</li>
+      <li class="next">&#10095;</li>
+      <li>August<br><span style="font-size:18px">2017</span></li>
+    </ul>
+  </div>
+
+  <ul class="jours_sem">
+    <li>Mo</li>
+    <li>Tu</li>
+    <li>We</li>
+    <li>Th</li>
+    <li>Fr</li>
+    <li>Sa</li>
+    <li>Su</li>
   </ul>
-</div>
 
-<ul class="jours_sem">
-  <li>Mo</li>
-  <li>Tu</li>
-  <li>We</li>
-  <li>Th</li>
-  <li>Fr</li>
-  <li>Sa</li>
-  <li>Su</li>
-</ul>
-
-<ul class="jours">
-  <li>1</li>
-  <li>2</li>
-  <li>3</li>
-  <li>4</li>
-  <li>5</li>
-  <li>6</li>
-  <li>7</li>
-  <li>8</li>
-  <li>9</li>
-  <li><span class="active">10</span></li>
-  <li>11</li>
-  ...etc
-</ul>
+  <ul class="jours">
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+    <li>5</li>
+    <li>6</li>
+    <li>7</li>
+    <li>8</li>
+    <li>9</li>
+    <li><span class="active">10</span></li>
+    <li>11</li>
+    ...etc
+  </ul>
+  <footer>
     
+
+  </footer>
 </body>
+
 </html>
